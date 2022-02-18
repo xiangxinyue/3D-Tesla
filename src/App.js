72ce7d6b24616b239327dc59075b7b4295acd749
Xinyue Xiang <xxiang2@ualberta.ca>
2022-02-18 11:47:08 -0500

    feat: Building a Scene with reactthreefiber

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import * as THREE from 'three';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { useRef } from 'react';
 
-function App() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const renderer = new THREE.WebGLRenderer(); 
-
-  renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-  );
-  
-  document.body.innerHTML = '';
-  document.body.appendChild(renderer.domElement);
-
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({
-    color: 'blue'
+const Box = () => {
+  const ref = useRef();
+  useFrame(state => {
+    console.log(state);
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
   });
-  camera.position.z = 5;
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
-
-  animate();
-
-  window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-  }
-  );
 
   return (
-    null
+    <mesh ref = {ref}>
+      <boxBufferGeometry/>
+      <meshBasicMaterial color='blue'/>
+    </mesh>
+  )
+}
+
+function App() {
+  return (
+    <div style={{height: '100vh', width: '100vw'}}>
+      <Canvas style={{background: 'black'}}>
+      <Box/>
+      </Canvas>
+    </div>
+   
   );
 }
 
